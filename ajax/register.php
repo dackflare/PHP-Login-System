362dfb32419e73afc4fb74d3d5ec25dcbef0ab39
Bryan Lian <dackflare@hotmail.com>
2018-11-29 17:29:27 -0600

    setting up redirect

---
+++ ajax/register.php
@@ -0,0 +1,28 @@
+<?php 
+
+    // Allow the config
+    define('__CONFIG__', true);
+
+    // Require the config
+    require_once "../inc/config.php"; 
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Return data in json format
+        header('Content-Type: application/json');
+
+        $array = [];
+        
+        // Make sure the user does not exist already.
+
+        // Make sure the user CAN be added and IS added.
+
+        // Return the proper info to JavaScript to redirect us.
+
+        $return['redirect'] = '/index.php?this-was-a-redirect';
+
+        echo json_encode($return, JSON_PRETTY_PRINT); exit;
+    } else {
+        // kill script.  So somthing else.
+        exit('test');
+    }
+?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,17 @@ $(document)
     var _form = $(this);
     var _error = $(".js-error", _form);
 
-    var data = {
+    var dataObj = {
         email: $("input[type='email']", _form).val(),
         password: $("input[type='password']", _form).val()       
     }
 
-    if(data.email.length < 6) {
+    if(dataObj.email.length < 6) {
         _error  
             .text("Please enter a valid email address")
             .show();
         return false;
-    } else if (data.password.length < 11) {
+    } else if (dataObj.password.length < 11) {
         _error  
             .text("Please enter a password that is at least 11 characters long.")
             .show(); 
@@ -25,7 +25,28 @@ $(document)
     // Assuming the code gets this far, we can start the ajax process
     _error.hide();
 
-    console.log(data);
+    $.ajax({
+        type: 'POST',
+        url: '/ajax/register.php',
+        data: dataObj,
+        dataType: 'json',
+        async: trye,
+    })
+    .done(function ajaxDone(data) {
+        // whatever data is
+        console.log(data);
+        if(data.redirect !++ undefined) {
+            window.location = data.redirect;
+        }
+    })
+    .fail(function ajaxFailed(e) {
+        // this failed
+        console.log(e);        
+    })
+    .always(function ajaxAlwaysDoThis(data) {
+        // always do
+        console.log("Always");        
+    })
 
     return false;
 
