564c7b4d7eafbf7b1ad9a7a0f01c85524dbc327c
Bryan Lian <dackflare@hotmail.com>
2018-11-29 16:20:19 -0600

    added username and password

--- inc/classes/DB.php
+++ inc/classes/DB.php
@@ -13,12 +13,12 @@ class DB {
 
         try {
 
-            self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=3306;dbname=login_course', 'root', 'root' );
+            self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=3306;dbname=login_course', 'dackflare', 'gambit1234' );
             self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
             self::$con->setAttribute( PDO::ATTR_PERSISTENT, false );
             
         } catch (PDOException $e) {
-            echo "Could not connect to database!!!."; exit;
+            echo "Could not connect to database."; exit;
         }
 
     }
